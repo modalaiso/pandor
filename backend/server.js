6164dfcc9534b2dd06c99863b4f5c1df4779d2c9
modalaiso <mobioisrael@hotmail.com>
2025-06-11 13:45:51 +0000

    bu

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'pandor.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
 app.get('/nos-services', (req, res) => {
