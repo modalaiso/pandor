40560677ce54d90cfc56f14bab5f5390a64905e3
modalaiso <134450637+modalaiso@users.noreply.github.com>
2025-06-12 16:54:50 +0000

    Update menu-toggle.js

--- frontend/js/menu-toggle.js
+++ frontend/js/menu-toggle.js
@@ -1,22 +1,12 @@
-//const toggle = document.querySelector(".menu-toggle");
-//const navLinks = document.querySelector(".nav_links");
-
-//toggle.addEventListener("click", () => {
-    //toggle.classList.toggle("active");
-    //navLinks.classList.toggle("open");
-//});
-
 const toggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav_links");
 
-// Ouvrir/fermer quand on clique sur le bouton
 toggle.addEventListener("click", (e) => {
     e.stopPropagation();
     toggle.classList.toggle("active");
     navLinks.classList.toggle("open");
 });
 
-// Fermer quand on clique ailleurs
 document.addEventListener("click", (e) => {
     if (!toggle.contains(e.target) && !navLinks.contains(e.target)) {
         toggle.classList.remove("active");
@@ -24,7 +14,6 @@ document.addEventListener("click", (e) => {
     }
 });
 
-// Fermer quand on scroll
 window.addEventListener("scroll", () => {
     toggle.classList.remove("active");
     navLinks.classList.remove("open");
